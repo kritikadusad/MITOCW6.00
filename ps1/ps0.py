9cad79ce6eeed5ec354700f51728c040a224da07
kritikadusad <kritikadusad@gmail.com>
2018-09-14 13:27:12 -0700

    Delete ps0.py

--- ps1/ps0.py
+++
@@ -1,8 +0,0 @@
-# Problem Set 0
-# Name: Kritika
-# Collaborators: None
-# Time Spent: 5 minutes
-
-dob = (raw_input('Enter your date of birth:'))
-lname = (raw_input('Enter your last name:'))
-print lname, dob
