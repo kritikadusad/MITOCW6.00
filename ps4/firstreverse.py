e220778a7aab1323418d7ff945e48ec9200733ad
Kritika Dusad <kritikadusad@gmail.com>
2018-09-14 17:46:57 -0700

    Work in progress: ps4 - crpytography

---
+++ ps4/firstreverse.py
@@ -0,0 +1,8 @@
+def FirstReverse(str): 
+	rev_str = ''
+    for i in range(len(str)):
+    	rev_str += str[i] 
+    return rev_str
+    
+# keep this function call here  
+print FirstReverse('I Love Code') 
